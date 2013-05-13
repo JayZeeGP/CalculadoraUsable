5f35782ce050e6a5ba9eefce06060f7a2d407c4e
José Carlos <jayzee1988@gmail.com>
2013-05-13 21:12:13 +0200

    Implementing coluring since 1988

--- logic.js
+++ logic.js
@@ -4,6 +4,78 @@
 	var secondNumber = 0;
 	var operator = "";
 
+	//a -> 97  -> green
+	//w -> 119 -> blue
+	//d -> 100 -> red
+	//s -> 115 -> yellow
+	//z -> 122 -> black
+	//x -> 120 -> white
+	function handleButtonPress(event){
+		var colorPressed = "";
+		switch(event.charCode){
+			case 97:
+				colorPressed = "green";
+				break;
+			case 119:
+				colorPressed = "blue";
+				break;
+			case 100:
+				colorPressed = "red";
+				break;
+			case 115:
+				colorPressed = "yellow";
+				break;
+			case 122:
+				colorPressed = "black";
+				break;
+			case 120:
+				colorPressed = "white";
+				break;				
+		}
+
+		if(colorPressed != ""){
+			nextMove(colorPressed);
+		}
+	}
+
+	function nextMove(colorPressed){
+		if(level == 0){
+			if(colorPressed == "green"){
+				selectRow(1);
+			}else if(colorPressed == "blue"){
+				selectRow(2);
+			}else if(colorPressed == "red"){
+				selectRow(3);
+			}
+		}
+	}
+
+	function selectRow(number){
+		for (var i = 0; i < document.querySelectorAll(".button").length; i++){
+			document.querySelectorAll(".button")[i].className = "button dark";	
+		}
+		switch(number){
+			case 1:
+				document.querySelector("#button7").className = "button blue";
+				document.querySelector("#button4").className = "button green";
+				document.querySelector("#button1").className = "button red";
+				document.querySelector("#button0").className = "button yellow";
+				break;
+			case 2:
+				document.querySelector("#button8").className = "button blue";
+				document.querySelector("#button5").className = "button green";
+				document.querySelector("#button2").className = "button red";
+				document.querySelector("#buttonDot").className = "button yellow";
+				break;
+			case 3:
+				document.querySelector("#button9").className = "button blue";
+				document.querySelector("#button6").className = "button green";
+				document.querySelector("#button3").className = "button red";
+				document.querySelector("#buttonNeg").className = "button yellow";
+				break;
+		}
+	}
+
 	function setColours(){
 		switch(level){
 			case 0: //Beggining the introduction of a number
@@ -78,52 +150,6 @@
   				break;  				
 		}
 	};
-
-	//a -> 97  -> green
-	//w -> 119 -> blue
-	//d -> 100 -> red
-	//s -> 115 -> yellow
-	//z -> 122 -> black
-	//x -> 120 -> white
-	function handleButtonPress(event){
-		var colorPressed = "";
-		switch(event.charCode){
-			case 97:
-				colorPressed = "green";
-				break;
-			case 119:
-				colorPressed = "blue";
-				break;
-			case 100:
-				colorPressed = "red";
-				break;
-			case 115:
-				colorPressed = "yellow";
-				break;
-			case 122:
-				colorPressed = "black";
-				break;
-			case 120:
-				colorPressed = "white";
-				break;				
-		}
-
-		if(colorPressed != ""){
-			nextMove(colorPressed);
-		}
-	}
-
-	function nextMove(colorPressed){
-		if(level == 0){
-			if(colorPressed == "green"){
-			selectRow(1);
-			}
-		}
-	}
-
-	function selectRow(number){
-		document.querySelectorAll(".button").className = "button dark";
-	}
 
 	setColours();
 	addEventListener("keypress", handleButtonPress,false);
